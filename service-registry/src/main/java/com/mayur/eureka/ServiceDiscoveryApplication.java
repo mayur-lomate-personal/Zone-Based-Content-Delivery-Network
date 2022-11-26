4308fbfe273edd5659809fd5e72589f94aa5399f
Mayur Lomate <mayurmlomate@gmail.com>
2022-11-26 13:01:28 +0530

    Initial commit

---
+++ service-registry/src/main/java/com/mayur/eureka/ServiceDiscoveryApplication.java
@@ -0,0 +1,17 @@
+package com.mayur.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author Marcos Barbero
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class ServiceDiscoveryApplication {
+
+    public static void main(String... args) {
+        SpringApplication.run(ServiceDiscoveryApplication.class, args);
+    }
+}
